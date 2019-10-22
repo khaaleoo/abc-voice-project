51d79a0431d9945844b935903bdaea4e5bffd791
Dinh Khai Tran <trandinhkhai10.1@gmail.com>
2019-10-22 13:08:35 +0700

    fix model for login

--- model/user.model.js
+++ model/user.model.js
@@ -1,6 +1,6 @@
 let db = require("../util/db");
 
-module.exports ={
+module.exports = {
   add: entity => {
     return db.add("user", entity);
   },
@@ -15,34 +15,16 @@ module.exports ={
       `select * from user where email='${email}' and password='${password}'`
     );
   },
-  findByEmail: email => {
-    return db.load(`select * from user where email='${email}'`).then(res=>{
-      console.log(res)
-      if(res.length>0){
-        return {
-          error:null,
-          data: res[0]
-        }
-      }
-      return {
-        error:'not found'
-      }
-    }).catch(e=>{
-      return {
-        error:e
-      }
-    })
-  },
+  findByEmail: email => db.load(`select * from user where email='${email}'`),
+
   verifyEmail: token => {
     return [];
     //tìm record chứa token sau đó update cột token => null và cột isActivated => true, nếu k tìm thấy return null
-    
   },
-  verifyRecoverToken: token =>db.load(`select * from user where token='${token}'`),
-  addRecoverToken:  (entity) => db.update('user', 'email', entity),
-  changePassword:(email,info) => {
+  verifyRecoverToken: token =>
+    db.load(`select * from user where token='${token}'`),
+  addRecoverToken: entity => db.update("user", "email", entity),
+  changePassword: (email, info) => {
     //tìm record với email tương ứng, sau đó sửa info cần thiết
-    
   }
 };
-
